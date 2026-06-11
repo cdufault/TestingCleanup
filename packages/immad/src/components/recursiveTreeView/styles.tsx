4c930f31a4822ecf80a26164f5823d6f5800d0b7
Chris Dufault <cdufault@esri.com>
2026-06-11 09:54:28 -0700

    Update highlight styles in tree view to use style file

--- packages/immad/src/components/recursiveTreeView/styles.tsx
+++ packages/immad/src/components/recursiveTreeView/styles.tsx
@@ -4,17 +4,22 @@ const useStyles = makeStyles({
     root: {
         flexGrow: 1,
         maxWidth: 300,
-        '&.MuiTreeItem-root.Mui-selected > .MuiTreeItem-content .MuiTreeItem-label:hover, .MuiTreeItem-root.Mui-selected:focus > .MuiTreeItem-content .MuiTreeItem-label, .MuiTreeItem-root.Mui-selected > .MuiTreeItem-content .MuiTreeItem-label':
+        // Theme highlight (secondary.main): '#54a8ff' | A shade lighter (secondary.light): '#85bffa'
+        '& .MuiTreeItem-content.Mui-selected, & .MuiTreeItem-content.Mui-selected.Mui-focused, & .MuiTreeItem-content.Mui-selected:hover':
             {
-                backgroundColor: 'green',
+                backgroundColor: '#333b42 !important',
+            },
+        '& .MuiTreeItem-content.Mui-selected .MuiTreeItem-label, & .MuiTreeItem-content.Mui-selected.Mui-focused .MuiTreeItem-label, & .MuiTreeItem-content.Mui-selected:hover .MuiTreeItem-label':
+            {
+                backgroundColor: 'transparent !important',
             },
     },
     rootNoHighlight: {
         flexGrow: 1,
         maxWidth: 300,
-        '&.MuiTreeItem-root.Mui-selected > .MuiTreeItem-content .MuiTreeItem-label:hover, .MuiTreeItem-root.Mui-selected:focus > .MuiTreeItem-content .MuiTreeItem-label, .MuiTreeItem-root.Mui-selected > .MuiTreeItem-content .MuiTreeItem-label':
+        '& .MuiTreeItem-content.Mui-selected, & .MuiTreeItem-content.Mui-selected.Mui-focused, & .MuiTreeItem-content.Mui-selected:hover':
             {
-                backgroundColor: 'transparent',
+                backgroundColor: 'transparent !important',
             },
     },
 });
